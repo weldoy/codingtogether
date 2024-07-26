bd09dce759bf05713e1baba85ca974581b288eb2
weldoy <132596720+weldoy@users.noreply.github.com>
2024-07-26 17:40:50 +0300

    calculator

---
+++ calculator.py
@@ -0,0 +1,19 @@
+def multiply():
+    first = int(input('enter the first digit: '))
+    second = int(input('enter the second digit: '))
+
+
+def divide():
+    pass
+
+
+def plus():
+    pass
+
+
+def minus():
+    pass
+
+
+def calculate():
+    pass
