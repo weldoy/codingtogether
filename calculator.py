a24ad8cc68a7737cad1cdd6f010e9b1cf37f9cdf
weldoy <132596720+weldoy@users.noreply.github.com>
2024-07-26 18:11:06 +0300

    calcul is ready

--- calculator.py
+++ calculator.py
@@ -1,11 +1,16 @@
 def multiply():
     first = int(input('enter the first digit: '))
     second = int(input('enter the second digit: '))
+    return first * second
 
 
 def divide():
     first = int(input('enter the first digit: '))
     second = int(input('enter the second digit: '))
+    if second != 0:
+        return first/second
+    else:
+        return 'u cant divide by zero'
 
 
 def plus():
@@ -14,11 +19,31 @@ def plus():
     summ = first + second
     return summ
 
+
 def minus():
     first = int(input('enter the first digit: '))
     second = int(input('enter the second digit: '))
     summ = first - second
     return summ
 
+
 def calculate():
-    pass
+    print(
+        '1 - minus\n',
+        '2 - plus\n',
+        '3 - divide\n',
+        '4 - multiply\n')
+    request = input('What is your req? :')
+    if request == '1' or request == 'minus':
+        return minus()
+    elif request == '2' or request == 'plus':
+        return plus()
+    elif request == '3' or request == 'divide':
+        return divide()
+    elif request == '4' or request == 'multiply':
+        return multiply()
+    else:
+        return 'bro, ti dolboeb. u menya nema takoy huini'
+
+
+print(calculate())
